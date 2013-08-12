5496a75843785766e8631c702521d0aea513714c
Igor Oleinikov <igor@oleinikov.ru>
2013-08-12 15:34:50 +0400

    Fixed bundling releated bug

--- HotelDiscount.Web/App_Start/BundleConfig.cs
+++ HotelDiscount.Web/App_Start/BundleConfig.cs
@@ -6,21 +6,20 @@ namespace HotelDiscount.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/Scripts").Include(
+            bundles.Add(new ScriptBundle("~/Content/ScriptsBundle").Include(
                 "~/Scripts/kendo/2013.2.716/kendo.web.min.js",
                 "~/Scripts/kendo/2013.2.716/cultures/kendo.culture.ru-RU.min.js",
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/knockout-{version}.js",
                 "~/Scripts/knockout-kendo.js",
                 "~/Scripts/linq.js"));
-            bundles.Add(new StyleBundle("~/Styles").Include(
-                "~/Content/kendo/2013.2.716/kendo.common.min.css",
-                //"~/Content/kendo/2013.2.716/kendo.default.min.css",
-                "~/Content/kendo/2013.2.716/kendo.metro.min.css",
+            bundles.Add(new StyleBundle("~/Content/StylesBundle").Include(
                 "~/Content/Styles/default.css"
                 ));
-
-            //BundleTable.EnableOptimizations = false;
+            bundles.Add(new StyleBundle("~/Content/kendo/2013.2.716/theme").Include(
+                "~/Content/kendo/2013.2.716/kendo.common.min.css",
+                "~/Content/kendo/2013.2.716/kendo.metro.min.css"
+                ));
         }
     }
 }
